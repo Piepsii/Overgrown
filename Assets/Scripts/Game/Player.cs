9ec418b6d477bd7cb902a54faae5dafe29a7710f
Paul Brandstetter <paulbrandstetter@tuta.io>
2022-01-11 21:31:46 +0100

    Player: camera controls
    Added Paul scene.
    Added camera controls with controllable fields to Player.
    Added Focus prefab to control camera center with.

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,14 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Overgrown.GameManager;
-using Overgrown.GameEnums;
 
 public class Player : MonoBehaviour
 {
-    private Camera cam;
-    private string leftMouseButton = "LMB";
-    private string rightMouseButton = "RMB";
+
+    Camera cam;
+    [SerializeField]
+    Transform focus = default;
+    [SerializeField, Range(1f, 200f)]
+    float distance = 150f;
+    [SerializeField, Range(1f, 300f)]
+    float minDistance = 30f, maxDistance = 200f;
+    [SerializeField, Range(1f, 360f)]
+    float rotationSpeed = 90f;
+    [SerializeField, Range(-89f, 89f)]
+    float minVerticalAngle = -30f, maxVerticalAngle = 60f;
+
+    Vector3 focusPoint, previousFocusPoint;
+    Vector2 orbitAngles = new Vector2(45f, 0f);
+    string leftMouseButton = "LMB";
+    string rightMouseButton = "RMB";
+
+    private void Awake()
+    {
+        focus = GameObject.FindGameObjectWithTag("Focus").transform;
+        focusPoint = focus.position;
+        transform.localRotation = Quaternion.Euler(orbitAngles);
+        cam = Camera.main;
+    }
 
     private void Start()
     {
@@ -16,12 +35,76 @@ public class Player : MonoBehaviour
         {
             GameManager.Instance.Player = this;
         }
-        cam = Camera.main;
+        CalculateCameraPosition();
     }
 
     private void Update()
     {
         CheckInput();
+    }
+
+    private void LateUpdate()
+    {
+        CalculateCameraPosition();
+    }
+
+    private void CalculateCameraPosition()
+    {
+        Quaternion lookRotation;
+        if (ManualRotation())
+        {
+            ConstrainAngles();
+            lookRotation = Quaternion.Euler(orbitAngles);
+        }
+        else
+        {
+            lookRotation = cam.transform.localRotation;
+        }
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        distance -= Input.mouseScrollDelta.y;
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+        Vector3 lookPosition = focusPoint - lookDirection * distance;
+        cam.transform.SetPositionAndRotation(lookPosition, lookRotation);
+    }
+
+    private void OnValidate()
+    {
+        if (maxVerticalAngle < minVerticalAngle)
+        {
+            maxVerticalAngle = minVerticalAngle;
+        }
+        if (maxDistance < minDistance)
+        {
+            maxDistance = minDistance;
+        }
+    }
+
+    private void ConstrainAngles()
+    {
+        orbitAngles.x = Mathf.Clamp(orbitAngles.x, minVerticalAngle, maxVerticalAngle);
+        if (orbitAngles.y >= 360f)
+        {
+            orbitAngles.y -= 360f;
+        }
+        else if (orbitAngles.y <= 0f)
+        {
+            orbitAngles.y += 360f;
+        }
+    }
+
+    private bool ManualRotation()
+    {
+        Vector2 input = new Vector2(
+            -Input.GetAxis("Mouse Y"),
+            Input.GetAxis("Mouse X")
+            );
+        const float e = 0.001f;
+        if ((input.x < -e || input.x > e || input.y < -e || input.y > e) && Input.GetButton(rightMouseButton))
+        {
+            orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+            return true;
+        }
+        return false;
     }
 
     private void CheckInput()
@@ -31,25 +114,13 @@ public class Player : MonoBehaviour
         var mouseY = Input.GetAxis("Mouse Y");
         Vector2 mousePosition = new Vector2(mouseX, mouseY);
         Vector3 worldPosition = cam.ScreenToWorldPoint(mousePosition);
-        if (PressedPrimaryButton())
+        if (Input.GetButtonDown(leftMouseButton))
         {
             levelManager.ToggleCellState(worldPosition);
-            Debug.Log("Toggle Cell State");
         }
-        else if (PressedSecondaryButton())
+        else if (Input.GetButtonDown(rightMouseButton))
         {
             levelManager.CrossCell(worldPosition);
-            Debug.Log("Cross out Cell");
-        }
-    }
-
-    private bool PressedPrimaryButton()
-    {
-        return Input.GetButtonDown(leftMouseButton);
-    }
-
-    private bool PressedSecondaryButton()
-    {
-        return Input.GetButtonDown(rightMouseButton);
+        } 
     }
 }
